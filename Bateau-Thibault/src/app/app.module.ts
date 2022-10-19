cbe3eeeef3d2163a194f5545af5ca51efc678a0b
luke <luke09@live.fr>
2022-10-19 09:41:05 +0200

    fix : headerComponent within a sharedModule

--- Bateau-Thibault/src/app/app.module.ts
+++ Bateau-Thibault/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SharedModule} from './sharedModule/shared.module';
+
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,SharedModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
