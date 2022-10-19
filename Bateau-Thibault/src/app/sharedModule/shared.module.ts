2648d46e30e2b27fc5906ebb64855fac6a4bc247
Pendelait <78800759+Pendelait@users.noreply.github.com>
2022-10-19 13:43:40 +0200

    Boat page start

--- Bateau-Thibault/src/app/sharedModule/shared.module.ts
+++ Bateau-Thibault/src/app/sharedModule/shared.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HeaderComponent} from '../header/header.component';
 import {IonicModule} from '@ionic/angular';
-
+import { RedirectionButtonComponent } from '../redirection-button/redirection-button.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent,RedirectionButtonComponent],
   imports: [
     CommonModule,
     IonicModule
   ],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent,RedirectionButtonComponent]
 })
 export class SharedModule { }
