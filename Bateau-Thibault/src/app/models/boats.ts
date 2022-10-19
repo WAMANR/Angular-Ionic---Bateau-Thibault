4ab8eb314943341bdb5c0b24e40556d44e377a74
Pendelait <78800759+Pendelait@users.noreply.github.com>
2022-10-19 16:58:26 +0200

    finish boat selection page

---
+++ Bateau-Thibault/src/app/models/boats.ts
@@ -0,0 +1,14 @@
+export class Boats{
+    name: string;
+    completeName: string;
+    src: string;
+    description:string;
+  
+    constructor(name: string, completeName: string, description: string, src: string) {
+      this.name = name;
+      this.completeName = completeName;
+      this.description = description;
+      this.src = src;
+    }
+  }
+  
